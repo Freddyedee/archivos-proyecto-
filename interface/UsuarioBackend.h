fc781799fb90cd17845605d76ec70eb063a654e0
Freddy Marcano <freddyaranguiz26@gmail.com>
2025-06-29 21:30:25 -0400

    add_newpath

---
+++ interface/UsuarioBackend.h
@@ -0,0 +1,22 @@
+#ifndef INTERFACE_USUARIOBACKEND_h
+#define INTERFACE_USUARIOBACKEND_h
+
+
+#include <QObject>
+#include <QString>
+#include "../../Core/Usuario.h"
+
+
+BackendUsuario()
+
+
+
+
+
+
+
+
+
+
+
+#endif //INTERFACE_USUARIOBACKEND_h
